5eea872727cd650378412d2d52b3f3ced6a483cd
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-07-20 22:53:36 -0500

    Variables y Constantes

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,20 @@
 <?php include 'includes/header.php';
 
 
+$nombre = "Ivan";
 
+$nombre = "Ivan 2";
+
+echo $nombre;
+var_dump($nombre);
+
+define('constante', "Este es el valor de la constante");
+echo constante;
+
+const constante2 = "Hola 2";
+echo constante2;
+
+$nombreCliente = "Pedro";
+$nombre_cliente = "Pedro";
 
 include 'includes/footer.php';
