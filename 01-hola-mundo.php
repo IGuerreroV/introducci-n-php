aec4c2589bff75f0bb302bcb47d7cd6b0afb3198
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-07-20 22:08:17 -0500

    Hola Mundo en PHP

---
+++ 01-hola-mundo.php
@@ -0,0 +1,14 @@
+<?php include 'includes/header.php';
+
+// echo "Hola mundo";
+// echo ("Hola mundo");
+
+// print "Hola mundo";
+
+//print("Hola mundo");
+
+// print_r("Hola mundo");
+
+// var_dump(20);
+
+include 'includes/footer.php';
