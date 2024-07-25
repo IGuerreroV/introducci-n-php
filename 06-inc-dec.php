beff94ec7bea8444171a23b26cd8cdb206ba4bbd
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-07-24 20:50:27 -0500

    Incremento y decremento

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,12 @@
 <?php include 'includes/header.php';
 
+$numero1 = 30;
+echo ++$numero1; // Incremento en 1
 
+$numero1 += 5;
+echo $numero1;
 
+$numero2 = 30;
+echo --$numero2; // Decremento en 1
 
 include 'includes/footer.php';
