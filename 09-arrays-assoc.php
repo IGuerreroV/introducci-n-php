4644a2e73e833654bbc5cc5afbd442c4cd76ca87
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-07-24 23:34:47 -0500

    Arreglos asociativos o Associative Arrays

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,25 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Ivan',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo'=> 'Premium',
+        'disponible' => 100
+    ]
+];
 
+echo "<pre>";
+var_dump($cliente['informacion']);
+echo '<pre>';
 
+// echo $cliente['nombre'];
+// echo $cliente['informacion']['disponible'];
+
+$cliente['codigo'] = 123456567;
+
+echo "<pre>";
+var_dump($cliente);
+echo '<pre>';
 
 include 'includes/footer.php';
