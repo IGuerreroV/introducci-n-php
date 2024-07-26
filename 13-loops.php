45c2bb0d49285760c23e012c5181df79eecc468a
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-07-25 23:30:02 -0500

    while, do while y foreach

--- 13-loops.php
+++ 13-loops.php
@@ -1,13 +1,13 @@
 <?php include 'includes/header.php';
 
 // While
-$i = 100; // Inicializador
+$i = 0; // Inicializador
 
-while($i < 10) {
+while($i < 10):
     echo $i . '<br>';
 
     $i++; // Incremento
-}
+endwhile;
 
 echo '<br>';
 // Do While
@@ -26,17 +26,33 @@ do {
  */
 
 // For Loop
-for($i = 1; $i < 100; $i++) {
-    if($i %  3 === 0 && $i % 5 === 0){
-        echo $i . ' - FIZZ BUZZ <br>';
-    } else if($i % 3 === 0) {
-        echo $i . ' - Fizz <br>';
-    } else if($i % 5 === 0) {
-        echo $i . ' - Buzz <br>';
-    } else {
-        echo $i . '<br>';
-    }
-    
+// for($i = 1; $i < 100; $i++) {
+//     if($i %  3 === 0 && $i % 5 === 0){
+//         echo $i . ' - FIZZ BUZZ <br>';
+//     } else if($i % 3 === 0) {
+//         echo $i . ' - Fizz <br>';
+//     } else if($i % 5 === 0) {
+//         echo $i . ' - Buzz <br>';
+//     } else {
+//         echo $i . '<br>';
+//     }
+// }
+
+// For Each
+$clientes = ['Pedro', 'Juan', 'Karen'];
+
+foreach($clientes as $cliente){
+    echo $cliente . '<br>';
+}
+
+$cliente = [
+    'nombre' => 'Ivan',
+    'saldo' => 200,
+    'tipo' => 'Premium'
+];
+
+foreach($cliente as $key => $valor){
+    echo $key . ' - ' . $valor . '<br>';
 }
 
 include 'includes/footer.php';
