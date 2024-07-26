ad3c8ce51a8dd82d1ccdb97da313984c918fc6c3
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-07-26 13:32:40 -0500

    Named Parameters

--- 15-funciones.php
+++ 15-funciones.php
@@ -4,11 +4,11 @@ include 'includes/header.php';
 
 
 
-function sumar(int $numero1 = 0, array $numero2) {
+function sumar(int $numero1 = 0, int $numero2) {
     echo $numero1 + $numero2;
 }
 
-sumar(10, []);
+sumar(numero1: 10, numero2: 20);
 
 
 include 'includes/footer.php';
